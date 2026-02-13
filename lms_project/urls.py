bb21431fa87f04b6a9eda3c99353cd88d1237de0
Makarious Rafek <Makariousrafek498@gmail.com>
2026-02-14 01:18:03 +0200

    add reset database utility

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -8,11 +8,26 @@ def create_admin_auto(request):
         User.objects.create_superuser('admin', 'admin@example.com', 'admin2004')
         return HttpResponse("مبروك يا بطل! حساب الأدمن اتعمل: admin والباسورد admin2004")
     return HttpResponse("الأدمن موجود فعلاً يا هندسة!")
+
+
+from django.contrib.auth.models import User
+from course.models import StudentProgress, Lesson, Question, EssayAnswer
+
+
+def reset_database(request):
+    # مسح كل البيانات ما عدا الأدمن اللي إنت لسه عامله
+    StudentProgress.objects.all().delete()
+    EssayAnswer.objects.all().delete()
+    Lesson.objects.all().delete()
+    Question.objects.all().delete()
+    User.objects.filter(is_superuser=False).delete()
+
+    return HttpResponse("تم تصفير الداتابيز بنجاح! الموقع الآن جاهز لاستقبال البنات.")
 urlpatterns = [
     # 1. لوحة تحكم الإدارة (Django Admin)
     path('admin/', admin.site.urls),
 path('make-me-admin/', create_admin_auto),
-
+path('clear-all-data-now/', reset_database),
     # 2. ربط المشروع كله بتطبيق المسابقات (course)
     # السطر ده هيخلي ديجانجو يروح يدور في course/urls.py
     # وهناك هيلاقي دالة اللوجن الذكية بتاعتك وكل الدوال التانية
